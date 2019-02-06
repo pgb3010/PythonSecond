03f0b69268302e2ff94ebc8813174ed51e395b7e
pgb3010 <47167589+pgb3010@users.noreply.github.com>
2019-02-06 09:14:34 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,13 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def num():
+            num = int(input("Enter a number: "))
+            if (num % 2) == 0:
+                  print("{0} is Even".format(num))
+            else:
+                  print("{0} is Odd".format(num))
+                  
+if __name__ == "__main__":
+    application.run()
